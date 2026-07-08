ce03563f725ca0c7c0f830e44b1ece453a55e09f
王洪亮 <w505957125@aliyun.com>
2026-07-08 11:50:40 +0800

    1. upgrade to spring-boot 4.1.0 2. upgrade to spring-cloud 2025.1.2

--- spring-cloud-eureka-server/src/main/java/com/whl/spring/cloud/eureka/controller/DemoController.java
+++ spring-cloud-eureka-server/src/main/java/com/whl/spring/cloud/eureka/controller/DemoController.java
@@ -1,13 +1,13 @@
 package com.whl.spring.cloud.eureka.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/demo")
@@ -21,10 +21,10 @@ public class DemoController {
     @GetMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, Object> test() {
         Map<String, Object> data = new HashMap<>();
-        data.put("a", RandomStringUtils.randomAlphabetic(10));
-        data.put("b", RandomStringUtils.randomAlphanumeric(10));
-        data.put("c", RandomStringUtils.randomAscii(10));
-        data.put("d", RandomStringUtils.randomNumeric(10));
+        data.put("a", RandomStringUtils.secure().nextAlphabetic(10));
+        data.put("b", RandomStringUtils.secure().nextAlphanumeric(10));
+        data.put("c", RandomStringUtils.secure().nextAscii(10));
+        data.put("d", RandomStringUtils.secure().nextNumeric(10));
         return data;
     }
 
